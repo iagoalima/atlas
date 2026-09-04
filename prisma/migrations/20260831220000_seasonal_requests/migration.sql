4bc8e369bc63bfeeef4b5f80ff886ba97215727d
Iago Lima <iagoalimadev@gmail.com>
2026-09-04 12:58:41 -0300

    fix: remove duplicate medal proof migration

--- prisma/migrations/20260831220000_seasonal_requests/migration.sql
+++ prisma/migrations/20260831220000_seasonal_requests/migration.sql
@@ -4,10 +4,5 @@ ALTER TABLE "guild_configs"
   ADD COLUMN "requestReviewChannelId" TEXT,
   ADD COLUMN "requestsOpen" BOOLEAN NOT NULL DEFAULT false;
 
-ALTER TABLE "ticket_proofs"
-  ADD COLUMN "medalId" TEXT;
-
 ALTER TABLE "tickets"
   ADD COLUMN "proofCollectionIndex" INTEGER NOT NULL DEFAULT 0;
-
-CREATE INDEX "ticket_proofs_medalId_idx" ON "ticket_proofs"("medalId");
